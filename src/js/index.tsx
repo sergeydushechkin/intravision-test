24b09246802ad07ecc018809f89254819a2cc12a
Sergey <54816584+sergeydushechkin@users.noreply.github.com>
2021-06-07 23:09:21 +0300

    Creates App component

---
+++ src/js/index.tsx
@@ -0,0 +1,9 @@
+import * as ReactDOM from "react-dom";
+import * as React from "react";
+
+import App from "./components/app";
+
+ReactDOM.render(
+    <App />,
+    document.querySelector(`#root`)
+);
